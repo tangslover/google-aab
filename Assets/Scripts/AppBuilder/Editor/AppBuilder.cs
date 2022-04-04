04c917053d5e7e63f1374a5481ef61a9972ea155
tangslover <tangslover@gmail.com>
2022-04-04 16:26:48 +0800

    上传 Android App Bundle 示例工程

---
+++ Assets/Scripts/AppBuilder/Editor/AppBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Google.Android.AppBundle.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public static class AppBuilder {
+    [MenuItem("Tools/Build App Bundle - Unity")]
+    public static void BuildForUnity() {
+        EditorUserBuildSettings.buildAppBundle = true;
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = false;
+
+        var assetPackConfig = new AssetPackConfig();
+        assetPackConfig.AddAssetsFolder("asset_base", "Assets/GoogleOutput", AssetPackDeliveryMode.InstallTime);
+
+        Bundletool.BuildBundle(new BuildPlayerOptions() {
+            target = BuildTarget.Android,
+            targetGroup = BuildTargetGroup.Android,
+            locationPathName = Path.Combine(Application.dataPath, "..", "Build/Android", "android-app-bundle.aab"),
+            scenes = new[] {
+                "Assets/Scenes/SampleScene.unity",
+            },
+            options = BuildOptions.None
+        }, assetPackConfig);
+    }
+
+    [MenuItem("Tools/Build App Bundle - Export Project")]
+    public static void BuildForGradle() {
+        EditorUserBuildSettings.buildAppBundle = false;
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
+
+        BuildPipeline.BuildPlayer(new[] {
+            "Assets/Scenes/SampleScene.unity",
+        }, Path.Combine(Application.dataPath, "..", "Build/Android", "android-app-bundle"), BuildTarget.Android, BuildOptions.None);
+    }
+
+    [MenuItem("Tools/Build Asset Bundle")]
+    public static void BuildAssetBundle() {
+        var outputPath = "Assets/GoogleOutput";
+        if (Directory.Exists(outputPath)) {
+            Directory.Delete(outputPath, true);
+        }
+
+        Directory.CreateDirectory(outputPath);
+
+        var assetBundleInfos = new AssetBundleBuild[] {
+            new AssetBundleBuild() {
+                assetBundleName = "ui/prefabs/launch.unity3d",
+                assetNames = new[] {
+                    "Assets/UI/Output/Prefabs/Launch.prefab"
+                },
+            }
+        };
+
+        BuildPipeline.BuildAssetBundles(outputPath, assetBundleInfos, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Google.Play.AssetDelivery;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+    [SerializeField] private Transform baseRoot;
+
+    // Start is called before the first frame update
+    void Start() {
+        var op = PlayAssetDelivery.RetrieveAssetPackAsync("asset_base");
+        op.Completed += OnRetrieveAssetPack;
+    }
+
+    private void OnRetrieveAssetPack(PlayAssetPackRequest request) {
+        var location = request.GetAssetLocation("ui/prefabs/launch.unity3d");
+        var assetBundle = AssetBundle.LoadFromFile(location.Path, 0, location.Offset);
+        var asset = assetBundle.LoadAsset<GameObject>("Launch");
+        var instance = Instantiate(asset, baseRoot);
+        instance.transform.localPosition = Vector3.zero;
+        instance.transform.rotation = Quaternion.identity;
+        instance.transform.localScale = Vector3.one;
+    }
+}
